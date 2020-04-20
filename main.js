a84e003177492e042a5ed62649e720bc2d0147bb
jasperhuangg <jasperhu@usc.edu>
2020-04-20 11:25:00 -0700

    backup

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var SpotifyWebApi = require("spotify-web-api-node");
 const { MongoClient } = require("mongodb");
 const mongoURI =
   "mongodb+srv://dbUser:Jasper99!@playlistwatcherproject-tjrwu.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM("");
+const $ = require("jquery")(window);
 
 app.use(express.urlencoded());
 app.set("view engine", "ejs");
@@ -27,6 +30,7 @@ var scopes = [
   "user-read-playback-state",
   "user-read-private",
   "user-read-email",
+  "user-modify-playback-state",
 ];
 
 var authorizeURL = spotifyApi.createAuthorizeURL(scopes);
@@ -84,18 +88,54 @@ app.get("/loginsuccess/", function (req, res, next) {
                       display_name: displayName,
                       spotify_uri: spotifyURI,
                       playlists: [],
+                      activeDeviceIDs: [],
                     });
                     console.log("Inserted.");
                   }
                 });
             }
           });
+
+          // update devices for the current user
+          spotifyApi.getMyDevices().then(
+            function (result) {
+              console.log(result.body.devices);
+              let devices = result.body.devices;
+              let dbDevices = [];
+              for (let i = 0; i < devices.length; i++) {
+                device = {};
+                device.id = devices[i].id;
+                device.name = devices[i].name;
+                dbDevices.push(device);
+              }
+              MongoClient.connect(mongoURI, function (err, db) {
+                let collection = db
+                  .db("PlaylistWatcherUsers")
+                  .collection("PlaylistWatcherUsers");
+                collection.findOneAndUpdate(
+                  { spotify_uri: spotifyURI },
+                  {
+                    $set: {
+                      activeDeviceIDs: dbDevices,
+                    },
+                  },
+                  function (err, doc) {
+                    if (err) console.error(err);
+                    else console.log("Updated devices.");
+                  }
+                );
+              });
+            },
+            function (err) {
+              console.log(err);
+            }
+          );
+          res.render(__dirname + "/loginsuccess.ejs"); // automatically closes itself
         },
         function (err) {
           console.log("Something went wrong!", err);
         }
       );
-      res.render(__dirname + "/loginsuccess.ejs"); // automatically closes itself
     },
     function (err) {
       console.log("Something went wrong!", err);
@@ -109,8 +149,12 @@ app.get("/watcher", function (req, res) {
 
 app.get("/watchlistupdate", function (req, res) {
   let spotify_user_uri = req.query.spotify_user_uri;
-  console.log("watchlistupdate spotify_user_uri: " + spotify_user_uri);
   GetUserWatchlist(spotify_user_uri, res);
+});
+
+app.get("/userdevices", function (req, res) {
+  let spotify_user_uri = req.query.spotify_user_uri;
+  GetUserDevices(spotify_user_uri, res);
 });
 
 // receive add playlist requests
@@ -128,7 +172,7 @@ app.post("/watcher", function (req, res) {
       console.log(data);
       let playlist_name = data.body.name;
       let img_uri = data.body.images[0].url;
-      let author = data.body;
+      let author = data.body.author;
       // check if playlist exists, if not insert
       MongoClient.connect(mongoURI, function (err, db) {
         if (err) throw err;
@@ -189,12 +233,11 @@ async function GetUserWatchlist(user_uri, res) {
           spotify_uri: user_uri,
         })
         .toArray(function (err, result) {
-          // TODO: result[0] is undefined when user first creates an account
           var dbPlaylists = result[0].playlists;
           var watchlist;
           if (dbPlaylists.length > 0) {
             watchlist = [];
-            // Get the name and img url for each playlist uri
+            // Get the name and img url for each playlist uri (maybe consider storing owner as well)
             for (let i = 0; i < dbPlaylists.length; i++) {
               let playlist = {};
               playlist.spotify_uri = dbPlaylists[i].spotify_uri;
@@ -202,14 +245,112 @@ async function GetUserWatchlist(user_uri, res) {
               playlist.img_uri = dbPlaylists[i].img_uri;
               watchlist.push(playlist);
             }
+            res.json(watchlist);
           } else {
             watchlist = "empty";
+            res.send(watchlist);
           }
-          console.log("Watchlist in GetUserWatchlist: " + watchlist);
-          res.json(watchlist);
+          // console.log("Watchlist in GetUserWatchlist: " + watchlist);
         });
     }
   });
 }
+
+function GetUserDevices(user_uri, res) {
+  MongoClient.connect(mongoURI, function (err, db) {
+    if (err) throw err;
+    else {
+      let collection = db
+        .db("PlaylistWatcherUsers")
+        .collection("PlaylistWatcherUsers");
+      collection
+        .find({
+          spotify_uri: user_uri,
+        })
+        .toArray(function (err, result) {
+          var dbDevices = result[0].activeDeviceIDs;
+          var devices;
+          if (dbDevices.length > 0) {
+            devices = [];
+            // Get the name and img url for each playlist uri (maybe consider storing owner as well)
+            for (let i = 0; i < dbDevices.length; i++) {
+              let device = {};
+              device.id = dbDevices[i].id;
+              device.name = dbDevices[i].name;
+              devices.push(device);
+            }
+            res.json(devices);
+          } else {
+            devices = "empty";
+            res.send(devices);
+          }
+          // console.log("Watchlist in GetUserWatchlist: " + watchlist);
+        });
+    }
+  });
+}
+
+app.post("/start-watching", function (req, res) {
+  let playlist_uri = req.body.playlist_uri;
+  let user_uri = req.body.spotify_user_uri;
+
+  // insert if user doesn't exist
+  MongoClient.connect(mongoURI, function (err, db) {
+    if (err) throw err;
+    else {
+      let collection = db
+        .db("PlaylistWatcherUsers")
+        .collection("PlaylistWatcherUsers");
+      collection
+        .find({ spotify_uri: user_uri })
+        .toArray(function (err, result) {
+          if (err) console.error(err);
+          else if (result.length) {
+            let device_id = result[0].activeDeviceIDs[0].id;
+            console.log(device_id);
+
+            // transfer playback to that device_id (make it active)
+            $.ajax({
+              headers: {
+                Authorization: "Bearer " + spotifyApi.getAccessToken(),
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              url: "https://api.spotify.com/v1/me/player",
+              type: "PUT",
+              data: JSON.stringify({ device_ids: [device_id] }),
+            });
+
+            // start playing the requested playlist on device with device_id:
+            $.ajax({
+              headers: {
+                Authorization: "Bearer " + spotifyApi.getAccessToken(),
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              url:
+                "https://api.spotify.com/v1/me/player/play?device_id=" +
+                device_id,
+              type: "PUT",
+              data: JSON.stringify({ context_uri: playlist_uri }),
+            });
+          }
+        });
+    }
+  });
+});
+
+app.post("/stop-watching", function (req, res) {
+  // stop user playback:
+  $.ajax({
+    headers: {
+      Authorization: "Bearer " + spotifyApi.getAccessToken(),
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    url: "https://api.spotify.com/v1/me/player/pause",
+    type: "PUT",
+  });
+});
 
 module.exports = app;
